e932ba4283a1201c9ed04a8a026222984a7bb0a3
colaola20 <soryo@farmingdale.edu>
2025-08-05 19:34:43 -0400

    feat: read post

--- src/pages/ReadPosts.jsx
+++ src/pages/ReadPosts.jsx
@@ -1,7 +1,42 @@
 import { supabase } from '../client'
+import { useState, useEffect } from 'react'
 
 const ReadPosts = (props) => {
+    const [posts, setPosts] = useState([])
 
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const {data} =await supabase
+                .from('Posts')
+                .select()
+                .order('created_at', {ascending: false});
+
+            setPosts(data)
+        }
+        fetchPosts();
+    }, [props])
+
+    return (
+        <div className="ReadPosts">
+            {
+                posts && posts.length > 0 ?
+                [...posts]
+                .sort((a, b) => a.id - b.id)
+                .map((post,index) => 
+                    <Post 
+                        key={post.id}
+                        id={post.id} 
+                        title={post.title}
+                        author={post.author}
+                        description={post.description}
+                        upvotes={post.upvotes}
+                        image_url={post.image_url}
+                        is_solved={posts.is_solved}
+                    />
+                ) : <h2>{'No Posts Yet 😞'}</h2>
+            }
+        </div>
+    )
 }
 
-export default 
+export default ReadPosts
